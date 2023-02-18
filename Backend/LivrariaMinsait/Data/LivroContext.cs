e5ae7eeaf42c703a1804ade8a97f4de2904fd0b3
Rennan Kelson <rennankelson@hotmail.com>
2023-02-17 21:24:31 -0300

    Criação de diretorios e entidade

---
+++ Backend/LivrariaMinsait/Data/LivroContext.cs
@@ -0,0 +1,13 @@
+﻿using LivrariaMinsait.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LivrariaMinsait.Context
+{
+    public class LivroContext : DbContext
+    {
+        public LivroContext(DbContextOptions<LivroContext> options) : base(options)
+        {
+        }
+        public DbSet<Livro> Livros { get; set; }
+    }
+}
